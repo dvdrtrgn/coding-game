edbc217da0d1832d243b60fd28313996b4e7d7f6
dvdrtrgn <dvdrtrgn@gmail.com>
2016-11-08 23:33:29 -0600

    refine mars

--- mars1.js
+++ mars1.js
@@ -1,13 +1,14 @@
-var error = function (...arr) {
+var prerr = function (...arr) {
   printErr(arr.map(a => a.join && a.join(':') || a).join(' | '));
 };
 var R = () => readline().split(' ');
 var RN = () => R().map(Number);
 
-var POINTS = RN(); // points used to draw the surface
+// X/Y coordinates used to draw the surface
+var POINTS = RN();
 for (var i = 0; i < POINTS; i++) {
   var [landX, landY] = RN();
-  error(['xy', landX, landY]); // X/Y coordinates each point
+  prerr('xy', [landX, landY]);
 }
 var V = -1;
 // game loop
@@ -16,6 +17,6 @@ while (true) {
   // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
 
   var rez = Math.min(((V += 0.24) | 0), 4);
-  error(['xy', X, Y], [hMps, vMps], ['gas', fuel], [rotate, power]);
+  prerr(['xy', X, Y], [hMps, vMps], ['gas', fuel], [rotate, power]);
   print('0 ' + rez); // [[rotation] [power]]
 }
