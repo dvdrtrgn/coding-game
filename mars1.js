c13ed1e90be382d3c1e46e295da00912d71e8962
dvdrtrgn <dvdrtrgn@gmail.com>
2016-11-10 22:31:23 -0600

    enhance mars

--- mars1.js
+++ mars1.js
@@ -1,35 +1,57 @@
-var prerr = function (...arr) {
+function sp2pow(speed, dist, adj = 9.6) {
+  var kms = dist / 1000;
+  var mag = Math.round((kms * adj + speed) / -adj);
+  var pow = Math.min(Math.max(mag, 0), 4);
+  //prerr('sp2pow', [speed, dist], [mag, pow]);
+  return pow;
+}
+
+function prerr(...arr) {
   printErr(arr.map(a => a.join && a.join(':') || a).join(' | '));
-};
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// init vars
 var R = () => readline().split(' ');
 var RN = () => R().map(Number);
 
 // X/Y coordinates used to draw the surface
 var POINTS = Array(RN().pop()).fill().map(RN);
+var x2alt = x => POINTS.reduce((a, b) => (x > a[0]) ? a : b)[1];
 
-function x2alt(x) {
-  var rez = POINTS.reduce((a, b) => (x > a[0]) ? a : b);
-  return rez[1];
-}
-
-function sp2pow(speed, dist, adj = 9) {
-  var kms = dist / 1000;
-  var mag = Math.round((kms * adj + speed) / -adj);
-  var pow = Math.min(Math.max(mag, 0), 4);
-  prerr('sp2pow', [speed, dist], [mag, pow]);
-  return pow;
-}
-
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // game loop
-landY = '';
+var Altitude;
 
 while (true) {
   var [X, Y, hMps, vMps, fuel, rotate, power] = RN();
-  // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
   prerr(['xy', X, Y], [hMps, vMps], ['gas', fuel], [rotate, power]);
-  landY = landY || x2alt(X);
+  // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
 
-  var dist = (Y - landY);
-  print('0 ' + sp2pow(vMps, dist)); // [[rotation] [power]]
-  //print('0 ' + vMps < -39 ? '4' : '0');
+  Altitude = Altitude || x2alt(X);
+
+  var pow = sp2pow(vMps, (Y - Altitude));
+  var rot = '0';
+
+  print(rot + ' ' + pow); // [[rotation] [power]]
+  //print('0 ' + (vMps < -39 ? '4' : '0'));
 }
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
--- mars2.js
+++ mars2.js
@@ -1,25 +1,80 @@
-var surfaceN = parseInt(readline()); // the number of points used to draw the surface of Mars.
-for (var i = 0; i < surfaceN; i++) {
-  var inputs = readline().split(' ');
-  var landX = parseInt(inputs[0]); // X coordinate of a surface point. (0 to 6999)
-  var landY = parseInt(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+function findFlats(where) {
+  var arr = []; // find flat areas
+  POINTS.reduce(function (a, b, i) {
+    if (a[1] === b[1]) arr.push([a[0], b[0]]);
+    return b; // seek consecutive Xs with same Y
+  });
+  if (where) arr = arr.reduce(function (a, b) {
+    return (where < ((a[1] + b[0]) / 2)) ? a : b;
+  }); // get closest flat
+  return arr;
 }
 
+function sp2pow(speed, dist, adj = 9.6) {
+  var kms = dist / 1000;
+  var mag = Math.round((kms * adj + speed) / -adj);
+  var pow = Math.min(Math.max(mag, 0), 4);
+  //prerr('sp2pow', [speed, dist], [mag, pow]);
+  return pow;
+}
+
+function prerr(...arr) {
+  printErr(arr.map(a => a.join && a.join(':') || a).join(' | '));
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// init vars
+var R = () => readline().split(' ');
+var RN = () => R().map(Number);
+
+// X/Y coordinates used to draw the surface
+var POINTS = Array(RN().pop()).fill().map(RN);
+var x2alt = x => POINTS.reduce((a, b) => (x > a[0]) ? a : b)[1];
+
+
+function seekFlat(x) {
+  // adjust angle according to how far from x1 and x2
+  if (x > LANDING[0] && x < LANDING[1]) 'all good';
+}
+
+function determAng(x) {
+  var z = x - 4440;
+  return Math.min(3, Math.round(z / 150));
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // game loop
+var Altitude;
+var Landing;
+var V = 2.1;
+
 while (true) {
-  var inputs = readline().split(' ');
-  var X = parseInt(inputs[0]);
-  var Y = parseInt(inputs[1]);
-  var hSpeed = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
-  var vSpeed = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
-  var fuel = parseInt(inputs[4]); // the quantity of remaining fuel in liters.
-  var rotate = parseInt(inputs[5]); // the rotation angle in degrees (-90 to 90).
-  var power = parseInt(inputs[6]); // the thrust power (0 to 4).
+  var [X, Y, hMps, vMps, fuel, rotate, power] = RN();
+  prerr(['xy', X, Y], [hMps, vMps], ['gas', fuel], [rotate, power]);
+  // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
 
-  // Write an action using print()
-  // To debug: printErr('Debug messages...');
+  Altitude = Altitude || x2alt(X);
+  Landing = Landing || findFlats(X);
 
+  var pow = sp2pow(vMps, (Y - Altitude));
+  var rot = determAng(X);
 
-  // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-  print('-20 3');
+  print(rot + ' ' + pow); // [[rotation] [power]]
 }
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
