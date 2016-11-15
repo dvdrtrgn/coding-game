8a8294297c6d48b7c644069ccf5f28b41872fd2b
dvdrtrgn <dvdrtrgn@gmail.com>
2016-11-15 00:00:26 -0600

    refine craft

--- mars/craft.js
+++ mars/craft.js
@@ -1,5 +1,9 @@
 var W = window;
 var C = W.console;
+var avgvel = 1.6;
+var getDist = (t, av) => Math.pow(t, 2) * (av || avgvel);
+var getDiff = (t1, t2) => getDist(t2 || t1 + 1) - getDist(t1);
+var setDefer = (fn) => W.setTimeout(fn, 1);
 
 $.fn.setCenter = function (x, y) {
   var me = $(this);
@@ -8,38 +12,44 @@ $.fn.setCenter = function (x, y) {
     top: y - me.height() / 2,
   });
   return this;
-}
+};
+$.fn.setAnima = function (b) {
+  var me = $(this);
+  b = `${b ? 'add' : 'remove'}Class`;
+  setDefer(() => me[b]('ani'));
+  return this;
+};
 
-function makeCoord(x, y) {
-  var obj, ele;
-
-  function useEvt(evt) {
+function makeCoord(x, y, ele) {
+  if ('offsetX' in x) {
     ele = x.target;
     y = x.offsetY;
     x = x.offsetX;
   }
-  if ('offsetX' in x) useEvt(x);
-  obj = {
+  let Api = {
     x, y, ele
   };
-  return obj;
+  return Api;
 }
 
-function makeTimer(fn, ms) {
-  var start = Date.now();
-  var token;
-  var Api = {
+function makeTimer(cback, delay) {
+  var start, token, tick;
+  let Api = {
+    tick: () => tick++,
     read: () => Date.now() - start,
+    gain: () => delay,
     set: function (fn, ms) {
+      this.clear();
+      [cback, delay] = [fn || cback, ms || delay];
       start = Date.now();
-      window.clearInterval(token);
-      token = window.setInterval(fn, ms);
+      token = window.setInterval(cback, delay);
     },
     clear: function () {
       window.clearInterval(token);
+      tick = 0;
     },
   };
-  if (fn && ms) Api.set(fn, ms);
+  if (cback && delay) Api.set(cback, delay);
   return Api;
 }
 
@@ -47,39 +57,35 @@ var Craft = (function () {
   var box = $('#Space');
   var rok = $('#Craft');
   var clock = makeTimer();
-  var height;
-  var avgvel = 16;
-  var times;
-  var coord;
   var ground = box.height() - rok.height() / 2.5;
+  var lower;
+  var coord;
 
-  var getDist = (t, av) => Math.pow(t, 2) * (av || avgvel);
-  var getDiff = (t1, t2) => getDist(t2 || t1 + 1) - getDist(t1);
-
-  function drop() {
-    times++;
-    rok.setCenter(coord.x, height).addClass('ani');
-    height = Math.min(height + getDiff(times), ground);
-    if (height >= ground) {
+  function dropping() {
+    if (ground <= lower) {
       clock.clear();
-      rok.setCenter(coord.x, height);
+      rok.setCenter(coord.x, lower)
+      return rok.setAnima(0);
     }
-    C.log(clock.read(), height, times);
+    rok.setCenter(coord.x, lower);
+    lower += getDiff(clock.tick());
+    lower = Math.min(lower, ground) | 0;
+    C.log(clock.read(), lower, clock.gain());
   }
 
-  function thrust() {
-    times -= 4;
+  function thrusting() {
+    clock.set();
+    lower -= Math.pow(avgvel, 2);
   }
   box.on('click', function (evt) {
-    if (evt.delegateTarget !== evt.target) return;
+    if (evt.delegateTarget !== evt.target) throw 'click!';
     coord = makeCoord(evt);
-    rok.removeClass('ani').setCenter(coord.x, coord.y);
-    height = coord.y;
-    times = -1;
-    clock.set(drop, 100);
+    lower = coord.y;
+    clock.set(dropping, 100);
+    rok.setCenter(coord.x, coord.y).setAnima(1);
   });
 
-  $(W).on('keypress', thrust);
+  $(W).on('keypress', thrusting);
 
 }());
 
