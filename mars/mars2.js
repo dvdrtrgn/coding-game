018f6f8c4282ba538c74eb94a9f2404a11310aaf
dvdrtrgn <dvdrtrgn@gmail.com>
2016-11-24 00:13:14 -0600

    doc flats

--- mars/mars2.js
+++ mars/mars2.js
@@ -1,12 +1,12 @@
-function findFlats(where) {
+function findFlatNear(xpos) {
   var arr = []; // find flat areas
-  POINTS.reduce(function (a, b, i) {
-    if (a[1] === b[1]) arr.push([a[0], b[0]]);
-    return b; // seek consecutive Xs with same Y
+  POINTS.reduce(function (a, b, i) { // [xy] [xy] count
+    if (a[1] === b[1]) arr.push([a[0], b[0]]); // Xs w/consecutive Ys
+    return b;
   });
-  if (where) arr = arr.reduce(function (a, b) {
-    return (where < mid(a[1], b[0])) ? a : b;
-  }); // get closest flat
+  if (xpos) arr = arr.reduce(function (a, b) {
+    return (xpos < mid(a[1], b[0])) ? a : b; // get closest flat
+  });
   return arr;
 }
 
@@ -28,10 +28,10 @@ var POINTS = Array(RN().pop()).fill().map(RN);
 var x2alt = x => POINTS.reduce((a, b) => (x > a[0]) ? a : b)[1];
 
 
-function getDistance(x) {
-  if (x < Landing[0]) return Landing[0] + 100;
-  if (x > Landing[1]) return Landing[1] - 100;
-  return x; // mid(Landing[0], Landing[1]);
+function getDistance(x, flat) {
+  if (x < flat[0]) return flat[0] + 100;
+  if (x > flat[1]) return flat[1] - 100;
+  return x; // mid(flat[0], flat[1]);
 }
 // adjust angle according to how far from x1 and x2
 
@@ -66,7 +66,7 @@ while (true) {
   // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
 
   Altitude = Altitude || x2alt(X);
-  Landing = Landing || findFlats(X);
+  Landing = Landing || findFlatNear(X);
   Target = Target || getDistance(X);
 
   prerr(['Altitude', Altitude], ['Landing', Landing], ['Target', Target]);
