22bd0803568b882ff86db76f7dbe06681bc0c474
dvdrtrgn <dvdrtrgn@gmail.com>
2016-11-25 21:57:33 -0600

    streamline

--- viral.js
+++ viral.js
@@ -2,22 +2,31 @@
 function dump(root, meth) {
   var obj = (meth ? root[meth] : root);
   var fun = (typeof obj === 'function');
-  console.info([meth, fun ? obj() : obj]);
+  var out = (fun ? obj() : obj);
+  console.info([root.getId(), meth, out, out + '']);
 }
 
 function makeNode(name, parent) {
   var children = []
   var obj = {
+    name: name,
     getId: () => name,
     getParent: () => parent,
     addChild: (node) => children.push(node),
     getChildren: () => children.slice(),
-    listChildren: () => children.map(o => o.toString()),
+    getTop: (parent = obj) => {
+      while (parent.getParent()) parent = parent.getParent();
+      return parent;
+    },
+    listChildren: () => {
+      var out = children.map(o => o.toString());
+      return out.length ? `+[${out}]` : '';
+    },
     valueOf: () => ({
       [name]: obj
     }),
     toString: function () {
-      var arr = [name, ' @', obj.getDepth(), '+[', obj.listChildren(), ']'];
+      var arr = [name, ' @', obj.getDepth(), obj.listChildren()];
       return arr.join('');
     },
     getDepth: function (depth = 0, parent = obj) {
@@ -34,14 +43,15 @@ function makeNode(name, parent) {
 function testParent() {
   var a = makeNode('foo');
   var b = makeNode('bar', a);
-  var c = makeNode('baz', a);
+  var c = makeNode('baz', b);
+  dump(a);
+  dump(a, 'getId');
+  dump(a, 'valueOf');
+  dump(a, 'toString');
+  dump(a, 'getChildren');
+  dump(b, 'listChildren');
+  dump(c, 'getTop');
   return a;
 }
 
 var par = testParent();
-dump(par);
-dump(par, 'valueOf');
-dump(par, 'toString');
-dump(par, 'getId');
-dump(par, 'getChildren');
-dump(par, 'listChildren');
