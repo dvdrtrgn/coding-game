4cad6614afa27e6f850ccf1fb70fb53b60598c06
dvdrtrgn <dvdrtrgn@gmail.com>
2016-12-07 22:36:56 -0600

    compile wip: mars2

---
+++ mars/craft.js
@@ -0,0 +1,102 @@
+var W = window;
+var C = W.console;
+var gravity = 32;
+var secseg = 10;
+var avgvel = gravity / 2 / secseg;
+var getDist = (t, av) => Math.pow(t, 2) * (av || avgvel);
+var getDiff = (t1, t2) => getDist(t2 || t1 + 1) - getDist(t1);
+var setDefer = (fn) => W.setTimeout(fn, 1);
+
+$.fn.setCenter = function (x, y) {
+  var me = $(this);
+  me.css({
+    left: x - me.width() / 2,
+    top: y - me.height() / 2,
+  });
+  return this;
+};
+$.fn.setAnima = function (b) {
+  var me = $(this);
+  b = `${b ? 'add' : 'remove'}Class`;
+  setDefer(() => me[b]('ani'));
+  return this;
+};
+
+function makeCoord(x, y, ele) {
+  if ('offsetX' in x) {
+    ele = x.target;
+    y = x.offsetY;
+    x = x.offsetX;
+  }
+  let Api = {
+    x, y, ele
+  };
+  return Api;
+}
+
+function makeTimer(cback, delay) {
+  var start, token, tick;
+  let Api = {
+    tick: () => tick++,
+    read: () => Date.now() - start,
+    gain: () => delay,
+    set: function (fn, ms) {
+      this.clear();
+      [cback, delay] = [fn || cback, ms || delay];
+      start = Date.now();
+      token = window.setInterval(cback, delay);
+    },
+    clear: function () {
+      window.clearInterval(token);
+      tick = 0;
+    },
+  };
+  if (cback && delay) Api.set(cback, delay);
+  return Api;
+}
+
+var Craft = (function () {
+  var box = $('#Space');
+  var rok = $('#Craft');
+  var clock = makeTimer();
+  var ground = box.height() - rok.height() / 2.5;
+  var lower;
+  var coord;
+
+  function dropping() {
+    if (ground <= lower) {
+      clock.clear();
+      rok.setCenter(coord.x, lower)
+      return rok.setAnima(0);
+    }
+    rok.setCenter(coord.x, lower);
+    lower += getDiff(clock.tick());
+    lower = Math.min(lower, ground);
+    C.log(clock.read(), lower | 0, clock.gain());
+  }
+
+  function thrusting() {
+    clock.set();
+    lower -= Math.pow(avgvel, 2);
+  }
+  box.on('click', function (evt) {
+    if (evt.delegateTarget !== evt.target) throw 'click!';
+    coord = makeCoord(evt);
+    lower = coord.y;
+    clock.set(dropping, 1000 / secseg);
+    rok.setCenter(coord.x, coord.y).setAnima(1);
+  });
+
+  $(W).on('keypress', thrusting);
+
+}());
+
+
+/*
+for each beat
+
+gravity pulls it down by m p sps
+
+angle determines x along with thrust
+
+*/
---
+++ mars/mars2.js
@@ -0,0 +1,103 @@
+var avgvel = 3.711 / 2 / 10;
+var getDist = (time) => Math.pow(time, 2) * avgvel;
+var getDiff = (t1, t2) => getDist(t2 || t1 + 1) - getDist(t1);
+
+var mid = (n1, n2) => (n1 + n2) / 2;
+var clip = (num, lo, hi) => Math.min(hi, Math.max(lo, num));
+var dual = (num) => num > 0 ? [-num, num] : [num, -num];
+var gate = (num, min) => (Math.abs(num) < min) ? 0 : num;
+var bound = (num, abs) => (abs = dual(abs)) && clip(num, abs[0], abs[1]);
+var round = (num) => Math.round(num);
+var signify = (num, fact) => Math.sign(num) * (fact || 1);
+
+function prerr(...arr) {
+  printErr(arr.map(a => a.join && a.join(':') || a).join(' | '));
+}
+
+function makePoint(x, y, nom) {
+  var obj = {
+    x, y
+  };
+  obj.toString = () => `${nom||''}: [${obj.x}, ${obj.y}]`;
+  obj.updateTo = (a, b) => (obj.x = a, obj.y = b, obj);
+  return obj;
+}
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// init vars
+var R = () => readline().split(' ');
+var RN = () => R().map(Number);
+
+function findZones(xys) {
+  var arr = []; // find flat areas
+  xys.reduce(function (xyA, xyB, i) {
+    if (xyA[1] === xyB[1]) { // consecutive Ys
+      arr.push([xyA[1], xyA[0], xyB[0]]);
+    }
+    return xyB;
+  });
+  return arr; // y x1 x2
+}
+
+// X/Y coordinates used to draw the surface
+var POINTS = Array(RN().pop()).fill().map(RN);
+var ZONES = findZones(POINTS);
+prerr(POINTS, '\nzones', ZONES);
+
+function getDistance(x, flat) {
+  var margin = 10;
+  if (x < flat[0]) return flat[0] + margin;
+  if (x > flat[1]) return flat[1] - margin;
+  return x; // mid(flat[0], flat[1]);
+}
+
+function findNearZone(xpos, zones) {
+  var arr = zones.reduce(function (yxxA, yxxB) {
+    return (xpos < mid(yxxA[2], yxxB[1])) ? yxxA : yxxB; // get closest flat
+  }).concat();
+  return {
+    alt: arr.shift(),
+    range: arr,
+  };
+}
+
+function calcThrust(speed) {
+  return clip(speed / -9, 0, 4) | 0;
+}
+
+function calcStop(sp, msp) {
+  var mod, rot, pow = 4;
+
+  rot = bound(Math.pow(msp.x, 5), 45);
+  mod = Math.log10(Math.pow(msp.y, 2));
+
+  if (mod > 1) rot /= mod;
+  if (Math.pow(sp.x, 2) < 10) pow = calcThrust(sp.y);
+  if (msp.y > 10) pow--;
+
+  return round(rot) + ' ' + round(pow);
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// game loop
+var LZ;
+var DISTS = makePoint(0, 0, 'Distance');
+var GO_TO = makePoint(0, 0, 'Go to');
+var SPEED = makePoint(0, 0, 'Speeds');
+var MODSP = makePoint(0, 0, 'Modify');
+var ALTER = makePoint(0, 0, 'Alter');
+
+while (true) {
+  var [X, Y, Xsp, Ysp, fuel, rotate, power] = RN();
+  // H/V speed(±m/s) & fuel(liters) & rotation(±90°) & thrust(0–4 Lps)
+  LZ = findNearZone(X, ZONES);
+
+  GO_TO.updateTo(getDistance(X, LZ.range), LZ.alt);
+  DISTS.updateTo(GO_TO.x - X, GO_TO.y - Y);
+  SPEED.updateTo(Xsp, Ysp);
+  MODSP.updateTo(signify(DISTS.x, 40), signify(DISTS.y, 10));
+  ALTER.updateTo(SPEED.x - MODSP.x, SPEED.y - MODSP.y);
+
+  prerr(GO_TO, DISTS, SPEED, ALTER);
+
+  print(calcStop(SPEED, ALTER)); // [[rotation] [power]]
+}
